49a59a0d50a959304a36c3d629c26da45fcac6f2
Hans Accola <Hansolo6283@gmail.com>
2020-10-06 09:48:18 -0500

    created games router

--- src/redux/sagas/games.saga.js
+++ src/redux/sagas/games.saga.js
@@ -1,29 +1,17 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-// worker Saga: will be fired on "REGISTER" actions
-function* registerUser(action) {
+function* fetchGames() {
   try {
-    // clear any existing error on the registration page
-    yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-
-    // passes the username and password from the payload to the server
-    yield axios.post('/api/user/register', action.payload);
-
-    // automatically log a user in after registration
-    yield put({ type: 'LOGIN', payload: action.payload });
-
-    // set to 'login' mode so they see the login screen
-    // after registration or after they log out
-    yield put({ type: 'SET_TO_LOGIN_MODE' });
+    let response = yield axios.get('/api/games');
+    console.log(response);
   } catch (error) {
-    console.log('Error with user registration:', error);
-    yield put({ type: 'REGISTRATION_FAILED' });
+    console.log('ERROR FETCHING GAMES', error);
   }
 }
 
-function* registrationSaga() {
-  yield takeLatest('REGISTER', registerUser);
+function* gamesSaga() {
+  yield takeLatest('FETCH_GAMES', fetchGames);
 }
 
-export default registrationSaga;
+export default gamesSaga;
