af961937ac52af2b1b489e1e4b793a2214bd75bb
Hans Accola <Hansolo6283@gmail.com>
2020-10-06 10:20:05 -0500

    finished get route for games, need to update in future

--- server/routes/games.router.js
+++ server/routes/games.router.js
@@ -2,19 +2,21 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+//the querytext in this route will need to be changed
+//as of now it's not getting the team names or logos, just displaying team id
+//need to change date column data type to time, time currently not displaying correctly
 router.get('/', (req, res) => {
-  // GET route code here
-  console.log('testing');
-});
-
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+    const queryText = `SELECT * FROM "games"
+                        WHERE "games".week = 3;`
+    pool.query(queryText)
+        .then((result) => {
+            console.log('ROUTER GAMES', result.rows);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('ERROR GETTING GAMES', error);
+            res.sendStatus(500); //internal server error
+        })
 });
 
 module.exports = router;
