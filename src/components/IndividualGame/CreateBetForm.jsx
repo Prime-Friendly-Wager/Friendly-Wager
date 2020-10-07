68bcf3bf9a73a78d604e42255218eda189433b09
Hans Accola <Hansolo6283@gmail.com>
2020-10-07 16:14:32 -0500

    created create bet form

--- src/components/IndividualGame/CreateBetForm.jsx
+++ src/components/IndividualGame/CreateBetForm.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { Button } from '@material-ui/core';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    createBetBtn: {
+      width: '50%',
+      marginLeft: '25%',
+      marginRight: '25%',
+      marginTop: '2em',
+    },
+  });
 
 function CreateBetForm(props) {
 
+    const classes = useStyles();
     const gameDetails = props.store.gameDetails
 
   return (
@@ -17,16 +28,19 @@ function CreateBetForm(props) {
         <FormControlLabel
           value={gameDetails.away_team}
           control={<Radio color="primary" />}
-          label={gameDetails.away_team}
+          label={gameDetails.away_team + ' ' + gameDetails.away_team_spread}
           labelPlacement="top"
         />
         <FormControlLabel
           value={gameDetails.home_team}
           control={<Radio color="primary" />}
-          label={gameDetails.home_team}
+          label={gameDetails.home_team + ' ' + gameDetails.home_team_spread}
           labelPlacement="top"
         />
       </RadioGroup>
+      <Button variant="contained" color="primary" className={classes.createBetBtn}>
+          Create Bet
+      </Button>
     </FormControl>
   );
 }
