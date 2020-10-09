0bf19f5619941179c81f5a551bcc2ac0d5026e03
Matt <Kraemermj1992@gmail.com>
2020-10-09 15:16:30 -0500

    added open bets for board

--- src/redux/sagas/bets.saga.js
+++ src/redux/sagas/bets.saga.js
@@ -23,9 +23,23 @@ function* fetchGameOpenBets(action) {
   }
 }
 
+//requesting 2.1 bets
+function* fetchAllOpenBets() {
+  try {
+    const response = yield axios.get(`/api/bets/open`);
+
+    yield put({type: 'SAVE_OPEN_BETS', payload: response.data})
+
+  } catch (error) {
+    console.log('ERROR FETCHING INDIVIDUAL GAME OPEN BETS', error);
+  }
+}
+
+
 function* betsSaga() {
   yield takeLatest('POST_BET', postBet);
   yield takeLatest('FETCH_GAME_OPEN_BETS', fetchGameOpenBets);
+  yield takeLatest('FETCH_ALL_OPEN_BETS', fetchAllOpenBets);
 }
 
 export default betsSaga;
