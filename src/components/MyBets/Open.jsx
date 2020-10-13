97c867dc4d0e37282973623d1a35323456b10091
Hans Accola <Hansolo6283@gmail.com>
2020-10-13 11:58:38 -0500

    created delete dialogue to confirm deleting bet

--- src/components/MyBets/Open.jsx
+++ src/components/MyBets/Open.jsx
@@ -4,14 +4,28 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Paper, Typography, Button } from '@material-ui/core'
 import moment from 'moment';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 function Open(props) {
 
-    //deleting bet
-    const handleDelete = (id) => {
-      console.log('deleting bet:', id);
-      props.dispatch({ type: 'DELETE_BET', payload: id });
-    }
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  //deleting bet
+  const handleDelete = (id) => {
+    console.log('deleting bet:', id);
+    props.dispatch({ type: 'DELETE_BET', payload: id });
+    handleClose();
+  }
 
   return (
     <>
@@ -25,24 +39,41 @@ function Open(props) {
                 <TableCell align="center">Game</TableCell>
                 <TableCell align="left">My Bet</TableCell>
                 <TableCell align="left">Wager</TableCell>
+                <TableCell align="left">Delete</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-               {/* only displays your proposed bets */}
-               {props.store.betReducer.openBetReducer.filter(bet => bet.proposers_id === props.store.user.id).map(bet => (
+              {/* only displays your proposed bets */}
+              {props.store.betReducer.openBetReducer.filter(bet => bet.proposers_id === props.store.user.id).map(bet => (
                 <TableRow key={bet.id}>
-                  <TableCell align="left">{moment(bet.date_played).format("M/D")}</TableCell> 
+                  <TableCell align="left">{moment(bet.date_played).format("M/D")}</TableCell>
                   <TableCell align="left">{bet.away_team_abbr} @ {bet.home_team_abbr}</TableCell>
                   {/* checks if proposer is home team */}
-                  {bet.proposers_team_is_home_team ? 
-                    <TableCell align="left">{bet.home_team_name} {bet.home_team_spread}</TableCell> 
+                  {bet.proposers_team_is_home_team ?
+                    <TableCell align="left">{bet.home_team_name} {bet.home_team_spread}</TableCell>
                     :
                     <TableCell align="left">{bet.away_team_name} {bet.away_team_spread}</TableCell>
                   }
                   <TableCell align="left">{bet.wager}</TableCell>
                   <TableCell align="left">
-                      <DeleteForeverIcon color="secondary" onClick={() => handleDelete(bet.id)}/>
+                    <DeleteForeverIcon color="secondary" onClick={handleClickOpen} />
                   </TableCell>
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogTitle id="alert-dialog-title">{`Delete open bet?`}</DialogTitle>
+                    <DialogActions>
+                      <Button onClick={handleClose} color="primary">
+                        Cancel
+                          </Button>
+                      <Button onClick={() => handleDelete(bet.id)} color="primary" autoFocus>
+                        Yes
+                        </Button>
+                    </DialogActions>
+                  </Dialog>
                 </TableRow>
               ))}
             </TableBody>
